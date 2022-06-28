720ac7d52f0feb486bb61a8baeadd5ddd5d2f49e
armazac <aarondanks@gmail.com>
2022-06-28 21:17:20 +1000

    Try to suppress exit CTD

---
+++ Scripts/Game/GameMode/OVT_OverthrowSaveStruct.c
@@ -0,0 +1,12 @@
+[BaseContainerProps()]
+class OVT_OverthrowSaveStruct : SCR_MissionStruct
+{
+	override bool Serialize()
+	{
+		return true;
+	}
+	override bool Deserialize()
+	{
+		return true;
+	}
+}
