1b54c916908f6ab6b2d095be45d34160104c336d
armazac <armazac@gmail.com>
2023-10-04 01:38:14 +1100

    Move QRF to virtualization

--- Scripts/Game/Controllers/OccupyingFaction/OVT_QRFControllerComponent.c
+++ Scripts/Game/Controllers/OccupyingFaction/OVT_QRFControllerComponent.c
@@ -12,7 +12,7 @@ class OVT_QRFControllerComponent: OVT_Component
 			
 	int m_iTimer = 120000;
 	
-	ref array<ref EntityID> m_Groups;
+	ref array<int> m_Groups;
 	
 	protected const int UPDATE_FREQUENCY = 10000;
 	const int QRF_RANGE = 1000;
@@ -43,7 +43,7 @@ class OVT_QRFControllerComponent: OVT_Component
 		
 		GetGame().GetCallqueue().CallLater(CheckUpdateTimer, 1000, true, owner);		
 		
-		m_Groups = new array<ref EntityID>;
+		m_Groups = new array<int>;
 		SendTroops();
 		Replication.BumpMe();		
 		
@@ -70,19 +70,26 @@ class OVT_QRFControllerComponent: OVT_Component
 	void KillAll()
 	{
 		BaseWorld world = GetGame().GetWorld();
-		foreach(EntityID id : m_Groups)
+		OVT_VirtualizationManagerComponent virt = OVT_Global.GetVirtualization();
+		foreach(int id : m_Groups)
 		{
-			IEntity group = world.FindEntityByID(id);
-			if(!group) continue;
-			SCR_AIGroup aigroup = SCR_AIGroup.Cast(group);
-			if(!aigroup) continue;
-			autoptr array<AIAgent> agents = new array<AIAgent>;
-			aigroup.GetAgents(agents);
-			foreach(AIAgent agent : agents)
-			{
-				DamageManagerComponent damageManager = DamageManagerComponent.Cast(agent.FindComponent(DamageManagerComponent));
-				if (damageManager && damageManager.IsDamageHandlingEnabled())
-					damageManager.SetHealthScaled(0);
+			OVT_VirtualizedGroupData data = OVT_VirtualizedGroupData.Get(id);
+			if(data && data.isSpawned)
+			{			
+				IEntity group = world.FindEntityByID(data.spawnedEntity);
+				if(!group) continue;
+				SCR_AIGroup aigroup = SCR_AIGroup.Cast(group);
+				if(!aigroup) continue;
+				autoptr array<AIAgent> agents = new array<AIAgent>;
+				aigroup.GetAgents(agents);
+				foreach(AIAgent agent : agents)
+				{
+					DamageManagerComponent damageManager = DamageManagerComponent.Cast(agent.FindComponent(DamageManagerComponent));
+					if (damageManager && damageManager.IsDamageHandlingEnabled())
+						damageManager.SetHealthScaled(0);
+				}
+			}else{
+				data.remove = true;
 			}
 		}
 	}
@@ -94,19 +101,13 @@ class OVT_QRFControllerComponent: OVT_Component
 		m_aSpawnPositions.Clear();
 		
 		BaseWorld world = GetGame().GetWorld();
-		foreach(EntityID id : m_Groups)
+		foreach(int id : m_Groups)
 		{
-			IEntity group = world.FindEntityByID(id);
-			if(!group) continue;
-			SCR_AIGroup aigroup = SCR_AIGroup.Cast(group);
-			if(!aigroup) continue;
-			autoptr array<AIAgent> agents = new array<AIAgent>;
-			aigroup.GetAgents(agents);
-			foreach(AIAgent agent : agents)
-			{
-				SCR_EntityHelper.DeleteEntityAndChildren(agent);
+			OVT_VirtualizedGroupData data = OVT_VirtualizedGroupData.Get(id);
+			if(data)
+			{	
+				data.remove = true;
 			}
-			SCR_EntityHelper.DeleteEntityAndChildren(aigroup);
 		}
 		
 		m_Groups.Clear();
@@ -120,30 +121,11 @@ class OVT_QRFControllerComponent: OVT_Component
 				
 		if(m_iTimer <= 0)
 		{
-			int enemyNum = 0;
-			int playerNum = 0;
-			int enemyTotal = 0;
+			OVT_VirtualizationManagerComponent virt = OVT_Global.GetVirtualization();
 			
-			array<AIAgent> groups();
-			GetGame().GetAIWorld().GetAIAgents(groups);
-			foreach(AIAgent group : groups)
-			{				
-				if(!group) continue;
-				IEntity entity = group.GetControlledEntity();
-				if(!entity) continue;
-				SCR_ChimeraCharacter character = SCR_ChimeraCharacter.Cast(entity);
-				if(!character) continue;
-				if(character.GetFactionKey() != OVT_Global.GetConfig().GetOccupyingFactionData().GetFactionKey()) continue;
-				float dist = vector.Distance(character.GetOrigin(),GetOwner().GetOrigin());
-				if(dist < QRF_POINT_RANGE)
-				{
-					enemyNum += 1;
-				}
-				if(dist < QRF_RANGE)
-				{
-					enemyTotal += 1;
-				}	
-			}
+			int enemyNum = virt.GetNumGroupsInRange(GetOwner().GetOrigin(), QRF_POINT_RANGE) * 4;
+			int playerNum = 0;
+			int enemyTotal = virt.GetNumGroupsInRange(GetOwner().GetOrigin(), QRF_RANGE) * 4;
 			
 			autoptr array<int> players = new array<int>;
 			PlayerManager mgr = GetGame().GetPlayerManager();
@@ -328,22 +310,17 @@ class OVT_QRFControllerComponent: OVT_Component
 	{
 		if(m_aSpawnQueue.Count() == 0) return;
 		
+		OVT_VirtualizationManagerComponent virt = OVT_Global.GetVirtualization();
+		
 		ResourceName res = m_aSpawnQueue[0];
 		vector pos = m_aSpawnPositions[0];
 		vector targetPos = m_aSpawnTargets[0];
 		
-		BaseWorld world = GetGame().GetWorld();
-			
-		EntitySpawnParams spawnParams = new EntitySpawnParams;
-		spawnParams.TransformMode = ETransformMode.WORLD;						
-		spawnParams.Transform[3] = pos;
-		IEntity group = GetGame().SpawnEntityPrefab(Resource.Load(res), world, spawnParams);
-				
-		SCR_AIGroup aigroup = SCR_AIGroup.Cast(group);
-		m_Groups.Insert(group.GetID());
+		array<vector> waypoints();
+		waypoints.Insert(targetPos);
 		
-		aigroup.AddWaypoint(OVT_Global.GetConfig().SpawnSearchAndDestroyWaypoint(targetPos));
-		aigroup.AddWaypoint(OVT_Global.GetConfig().SpawnDefendWaypoint(targetPos));
+		OVT_VirtualizedGroupData data = virt.Create(res, pos, waypoints, false, OVT_GroupOrder.ATTACK, OVT_GroupSpeed.RUNNING, false);		
+		m_Groups.Insert(data.id);		
 		
 		m_aSpawnQueue.Remove(0);
 		m_aSpawnPositions.Remove(0);
