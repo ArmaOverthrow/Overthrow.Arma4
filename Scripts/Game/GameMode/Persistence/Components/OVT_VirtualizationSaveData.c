6e6333716b639f159c719049b1cf28db8f3225bf
armazac <armazac@gmail.com>
2023-10-06 00:11:21 +1100

    Move virtualization to persistent scripted states

--- Scripts/Game/GameMode/Persistence/Components/OVT_VirtualizationSaveData.c
+++ Scripts/Game/GameMode/Persistence/Components/OVT_VirtualizationSaveData.c
@@ -1,3 +1,21 @@
+[
+    EPF_PersistentScriptedStateSettings(OVT_VirtualizedGroupData),
+    EDF_DbName.Automatic()
+]
+class OVT_VirtualizedGroupSaveData : EPF_ScriptedStateSaveData
+{
+    int id;					//
+	vector pos;				//Current position
+	ResourceName prefab; 	//Group prefab
+	ref array<vector> waypoints = new array<vector>; //the current waypoints for this group
+	OVT_GroupOrder order;	//Current order for this group (determines what type of waypoints are spawned)
+	bool isTrigger = false; //Group can spawn nearby virtualized AI like a player can
+	float altitude = 0;		//Current altitude above land
+	OVT_GroupSpeed speed;	//Travel speed (when virtualized)
+	bool cycleWaypoints = true;
+	int currentWaypoint = 0;
+}
+
 [EPF_ComponentSaveDataType(OVT_VirtualizationManagerComponent), BaseContainerProps()]
 class OVT_VirtualizationSaveDataClass : EPF_ComponentSaveDataClass
 {
@@ -6,14 +24,14 @@ class OVT_VirtualizationSaveDataClass : EPF_ComponentSaveDataClass
 [EDF_DbName.Automatic()]
 class OVT_VirtualizationSaveData : EPF_ComponentSaveData
 {
-	ref array<ref OVT_VirtualizedGroupData> m_aGroups;
+	ref array<string> m_aGroups;
 	
 	override EPF_EReadResult ReadFrom(IEntity owner, GenericComponent component, EPF_ComponentSaveDataClass attributes)
 	{		
 		OVT_VirtualizationManagerComponent virt = OVT_VirtualizationManagerComponent.Cast(component);
 		
-		m_aGroups = new array<ref OVT_VirtualizedGroupData>;
-		virt.GetGroupsArray(m_aGroups);
+		m_aGroups = new array<string>;
+		virt.GetGroupsIdArray(m_aGroups);
 		
 		return EPF_EReadResult.OK;
 	}
@@ -22,7 +40,7 @@ class OVT_VirtualizationSaveData : EPF_ComponentSaveData
 	{
 		OVT_VirtualizationManagerComponent virt = OVT_VirtualizationManagerComponent.Cast(component);
 			
-		virt.LoadGroupsArray(m_aGroups);		
+		virt.LoadGroupsIdArray(m_aGroups);
 				
 		return EPF_EApplyResult.OK;
 	}
