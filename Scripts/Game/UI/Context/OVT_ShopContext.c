2e466a5bd3a2d799f6ef8837ac1d610b39fbc6d8
Sparknutz <shawn.sperduti@gmail.com>
2025-03-03 15:07:18 -0800

    Update OVT_ShopContext.c
    This changes the way selling is disabled in shops. It creates a helper function determine sell button visibility. It is more modular and can be used in other shop instances in the future. I also added a print function for gun dealer sell multiplier for debugging.

--- Scripts/Game/UI/Context/OVT_ShopContext.c
+++ Scripts/Game/UI/Context/OVT_ShopContext.c
@@ -21,50 +21,109 @@ class OVT_ShopContext : OVT_UIContext
 		}
 	}
 	
+	//SPARKNUTZ CHANGED CODE
+	
 	override void OnShow()
 	{	
 		m_iPageNum = 0;	
 		
-		
+		// Set up the buy button
 		Widget buyButton = m_wRoot.FindAnyWidget("BuyButton");
 		SCR_InputButtonComponent action = SCR_InputButtonComponent.Cast(buyButton.FindHandler(SCR_InputButtonComponent));
-				
 		action.m_OnActivated.Insert(Buy);
 		
+		// Set up the sell button with updated visibility logic
 		Widget sellButton = m_wRoot.FindAnyWidget("SellButton");
-		//if(m_Shop.m_ShopType == OVT_ShopType.SHOP_GUNDEALER || m_Shop.m_ShopType == OVT_ShopType.SHOP_VEHICLE) - Original Changes by Chris
-		if(m_Shop.m_ShopType == OVT_ShopType.SHOP_VEHICLE)
+		bool showSellButton = ShouldShowSellButton(m_Shop.m_ShopType);
+		sellButton.SetVisible(showSellButton);
+		if (showSellButton)
 		{
-			sellButton.SetVisible(false);
-		}else{
-			if(m_Shop.m_ShopType == OVT_ShopType.SHOP_GUNDEALER && OVT_Global.GetConfig().m_Difficulty.gunDealerSellPriceMultiplier == 0)
-			{
-				sellButton.SetVisible(false);
-			}else{
-				SCR_InputButtonComponent sellAction = SCR_InputButtonComponent.Cast(sellButton.FindHandler(SCR_InputButtonComponent));
-			
-				sellAction.m_OnActivated.Insert(Sell);
-			}
+			SCR_InputButtonComponent sellAction = SCR_InputButtonComponent.Cast(sellButton.FindHandler(SCR_InputButtonComponent));
+			sellAction.m_OnActivated.Insert(Sell);
 		}
 		
-		
-		
+		// Set up the previous button
 		Widget prevButton = m_wRoot.FindAnyWidget("PrevButton");
 		SCR_InputButtonComponent btn = SCR_InputButtonComponent.Cast(prevButton.FindHandler(SCR_InputButtonComponent));
-		
 		btn.m_OnActivated.Insert(PreviousPage);
 		
+		// Set up the next button
 		Widget nextButton = m_wRoot.FindAnyWidget("NextButton");
 		btn = SCR_InputButtonComponent.Cast(nextButton.FindHandler(SCR_InputButtonComponent));
-		
 		btn.m_OnActivated.Insert(NextPage);
 		
+		// Set up the close button
 		Widget closeButton = m_wRoot.FindAnyWidget("CloseButton");
 		btn = SCR_InputButtonComponent.Cast(closeButton.FindHandler(SCR_InputButtonComponent));		
 		btn.m_OnActivated.Insert(CloseLayout);
 		
 		Refresh();		
 	}
+	
+	// Helper function to determine sell button visibility
+	private bool ShouldShowSellButton(OVT_ShopType shopType)
+	{
+		if (shopType == OVT_ShopType.SHOP_VEHICLE)
+		{
+			return false; // Hide for vehicle shops
+		}
+		if (shopType == OVT_ShopType.SHOP_GUNDEALER)
+		{
+			float multiplier = OVT_Global.GetConfig().m_Difficulty.gunDealerSellPriceMultiplier;
+			Print("gunDealerSellPriceMultiplier: " + multiplier); // Retain debug print
+			return multiplier != 0; // Show only if multiplier is not zero
+		}
+		return true; // Show for all other shop types
+	}
+	
+		//SPARKNUTZ CHANGED CODE
+	
+//	override void OnShow()
+//	{	
+//		m_iPageNum = 0;	
+//		
+//		
+//		Widget buyButton = m_wRoot.FindAnyWidget("BuyButton");
+//		SCR_InputButtonComponent action = SCR_InputButtonComponent.Cast(buyButton.FindHandler(SCR_InputButtonComponent));
+//				
+//		action.m_OnActivated.Insert(Buy);
+//		
+//		Widget sellButton = m_wRoot.FindAnyWidget("SellButton");
+//		//if(m_Shop.m_ShopType == OVT_ShopType.SHOP_GUNDEALER || m_Shop.m_ShopType == OVT_ShopType.SHOP_VEHICLE) - Original Changes by Chris
+//		if(m_Shop.m_ShopType == OVT_ShopType.SHOP_VEHICLE)
+//		{
+//			sellButton.SetVisible(false);
+//		}else{
+//			//Sparknutz - adding a print for debugging//
+//			Print("gunDealerSellPriceMultiplier: " + OVT_Global.GetConfig().m_Difficulty.gunDealerSellPriceMultiplier);
+//			if(m_Shop.m_ShopType == OVT_ShopType.SHOP_GUNDEALER && OVT_Global.GetConfig().m_Difficulty.gunDealerSellPriceMultiplier == 0)
+//			{
+//				sellButton.SetVisible(false);
+//			}else{
+//				SCR_InputButtonComponent sellAction = SCR_InputButtonComponent.Cast(sellButton.FindHandler(SCR_InputButtonComponent));
+//			
+//				sellAction.m_OnActivated.Insert(Sell);
+//			}
+//		}
+//		
+//		
+//		
+//		Widget prevButton = m_wRoot.FindAnyWidget("PrevButton");
+//		SCR_InputButtonComponent btn = SCR_InputButtonComponent.Cast(prevButton.FindHandler(SCR_InputButtonComponent));
+//		
+//		btn.m_OnActivated.Insert(PreviousPage);
+//		
+//		Widget nextButton = m_wRoot.FindAnyWidget("NextButton");
+//		btn = SCR_InputButtonComponent.Cast(nextButton.FindHandler(SCR_InputButtonComponent));
+//		
+//		btn.m_OnActivated.Insert(NextPage);
+//		
+//		Widget closeButton = m_wRoot.FindAnyWidget("CloseButton");
+//		btn = SCR_InputButtonComponent.Cast(closeButton.FindHandler(SCR_InputButtonComponent));		
+//		btn.m_OnActivated.Insert(CloseLayout);
+//		
+//		Refresh();		
+//	}
 	
 	void PreviousPage()
 	{
